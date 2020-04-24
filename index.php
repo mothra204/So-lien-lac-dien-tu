41264fd38d580fac8c726588548e0d0c970e456d
mothra204 <th.nam204@gmail.com>
2020-04-24 12:30:16 +0700

    file lam web

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php 
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SỔ LIÊN LẠC ĐIỂN TỬ HUTECH</title>
+  <link rel="stylesheet" type="text/css" href="css/styte.css" />
+  <link rel="shortcut icon" type="img/jpg" href="img/logohutech.jpg">
+</head>
+	<body>	
+ <div id="top_bar">
+  <?php 
+            if ($_SESSION['Role_ID'] == '1')
+            {
+          ?>
+              <h1><a href="url">NHAP DIEM</a></h1>
+              <h1><a href="url">XEM DIEM</a></h1>
+          <?php 
+            }else
+          ?>
+          <?php 
+            if ($_SESSION['Role_ID'] == '2')
+            {
+          ?>
+              <h1><a href="url">XEM DIEM</a></h1>
+          <?php 
+            }
+		  ?>
+</div> 
+		<div id="menu_top">
+			<ul>
+				<li><a href="index.html" title="Truy cập vào Trang Chủ" Class="active">Trang Chủ</a></li>
+				<li><a href="gioithieu.html" title="Truy cập vào Giới Thiệu">Giới Thiệu</a></li>
+				
+				<li><a href="http://localhost:8080/testlogin/url" title="Xem Điểm">Xem Điểm</a></li>
+				<li><a href="#" title="Thông Báo">Thông Báo</a></li>
+				<li><a href="detail.html" title="Hướng Dẫn">Hướng Dẫn</a></li>		
+			</ul>
+		</div>
+		<div id="banner">
+			<img src="img/hutech.png" />
+		</div>
+		<div id="main">
+			<div id="left">
+				<ul>
+					<li>
+						<img src="img/thongbao1.jpg" />
+						<h2><a href="https://www.hutech.edu.vn/tuyensinh/tin-tuc/tin-tuyen-sinh/14583411-chuoi-chuong-trinh-tu-van-tuyen-sinh-truc-tuyen-your-future-your-choice-se-bat-dau-tu-232" title="Tin tức">Your Futrue - Your Choice</a></h2>
+						Ngày 23/2/2020 tới đây, chương trình Tư vấn tuyển sinh trực tuyến 2020 với chủ đề “Your Future - Your Choice”...
+					</li>
+
+					<li>
+						<img src="img/thongbao2.jpg" />
+						<h2><a href="https://www.hutech.edu.vn/viendtn/tin-tuc/14583662-bo-truong-gd-dt-quyet-dinh-lui-thi-thpt-quoc-gia-va-thoi-diem-ket-thuc-nam-hoc" title="Tin tức">Thời gian thi THPT quốc gia</a></h2>
+						Bộ GD-ĐT quyết định lùi thi THPT quốc gia và thời điểm kết thúc năm học
+					</li>
+
+					<li>
+						<img src="img/thongbao3.png" />
+						<h2><a href="https://www.hutech.edu.vn/e-hutech/tin-tuc-su-kien/14583635-sinh-vien-hutech-tiep-tuc-nghi-hoc-den-het-ngay-293-de-phong-chong-dich-benh-covid-19" title="Tin tức">Dời thời gian nhập học trở lại</a></h2>
+						Sinh viên trường Đại học Công nghệ TP.HCM (HUTECH) sẽ tiếp tục nghỉ học đến hết ngày 29/3.
+					</li>
+				</ul> 
+			</div>
+			
+		</div>
+		<div id="footer">
+			<ul>
+				<li>
+					<a href="https://www.facebook.com/THN.Mothra">
+					Thông tin xin liên hệ
+					</a>
+				</li>
+			</ul>
+		</div>
+	</body>
+</html>
---
+++ login.php
@@ -0,0 +1,47 @@
+<?php 
+    session_start();
+    $host="localhost";
+    $user="root";
+    $passwork="";
+    $dbname="test";
+
+    $RoleID;
+
+     // Create connection
+    $conn = mysqli_connect($host,$user,$passwork,$dbname);
+     // Check connection
+    if ($conn->connect_error) 
+    {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    if(isset($_POST['uname'])){
+        $username = $_POST['uname'];
+        $pwk = $_POST['psw'];
+
+        $result = mysqli_query($conn, "Select * from account where Username = '".$username."' and password ='".$pwk."'");
+        $row=  mysqli_num_rows($result);
+        // Associative array
+        // Free result set
+
+        if($row==true){
+            
+            $row = $result -> fetch_array(MYSQLI_ASSOC);
+            $RoleID = $row['RoleID'];
+            if ($RoleID == 1 )
+                $_SESSION['Role_ID'] = '1';
+            elseif ($RoleID == 2 )
+                $_SESSION['Role_ID'] = '2';
+            
+            header("Location:http://localhost:8080/testlogin/index.php");
+            //if ($RoleID == 2 )
+             //   header("Location:http://google.com");
+        }
+        else{
+            echo "Kiểm tra lại tài khoản và mật khẩu . <a href='javascript: history.go(-1)'>Trở lại</a>";
+        }
+    }
+
+    
+
+?>
